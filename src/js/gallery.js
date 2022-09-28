6d9f449bc79603c36e55369596bfc109524c2bbe
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-29 00:01:03 +0300

    add css styles

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,20 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import axios from 'axios';
 
- import SimpleLightbox from 'simplelightbox';
- import 'simplelightbox/dist/simple-lightbox.min.css';
- import { Notify } from 'notiflix/build/notiflix-notify-aio';
- import debounce from 'lodash.debounce';
- import axios from 'axios';
- 
- const refs = {
-   formEl: document.querySelector(".search-form"),
-   searchBtnEl: document.querySelector(".submit-btn"),
-   galleryEl: document.querySelector('.gallery'),
-   inputEl: document.querySelector('input'),
-  }
+const refs = {
+  formEl: document.querySelector('.search-form'),
+  searchBtnEl: document.querySelector('.submit-btn'),
+  galleryEl: document.querySelector('.gallery'),
+  inputEl: document.querySelector('input'),
+};
+
+let queryWord = '';
 
 async function getImage(word, pageNumber) {
   try {
-    const response = await axios.get(`https://pixabay.com/api/?key=22104578-b37830bb47769ec8fcc7503cc&q=${word}&image_type=photo&pretty=true&page=${pageNumber}&per_page=40`);
+    const response = await axios.get(
+      `https://pixabay.com/api/?&pretty=true&page=${pageNumber}`,
+      {
+        params: {
+          key: '22104578-b37830bb47769ec8fcc7503cc',
+          image_type: 'photo',
+          per_page: 40,
+          q: word,
+          orientation: 'horizontal',
+          safesearch: true,
+
+        }
+      }
+  
+      );
     // console.log(response.data.hits);
     const linksArray = response.data.hits;
     return linksArray;
@@ -23,59 +38,60 @@ async function getImage(word, pageNumber) {
   }
 }
 
- function renderHTML(array) {const imagesMarkup = array.map(image => 
-  {return ` <li><a class = "gallery__item" href = "${image.largeImageURL}">
-  <img class = "gallery__image" alt = "${image.tags}" src = "${image.previewURL}"></a></li>`}
- ); 
- refs.galleryEl.insertAdjacentHTML('afterbegin',imagesMarkup.join(''));
+
+function renderHTML(array) {
+  const imagesMarkup = array.map(image => {
+    return ` <div class="photo-card">
+    <img src="${image.webformatURL}" alt = "${image.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${image.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${image.views}
+      </p>
+      <p class="info-item">
+      <b>Comments</b>
+      ${image.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${image.downloads}
+      </p>
+    </div>
+  </div>`;
+  });
+  refs.galleryEl.insertAdjacentHTML('afterbegin', imagesMarkup.join(''));
+  callLightbox();
   //  console.log(imagesMarkup);
+}
 
- };
-
-
-
-// getImage('cat', 1).then(linksArray => {renderHTML(linksArray)});
-getImage('cat', 2).then(linksArray => console.log(linksArray));
-// getImage('cat', 3).then(linksArray => console.log(linksArray));
-
-
-
-  
-refs.formEl.addEventListener('submit', onSubmit);
-
-let queryWord = "";
-
-function onSubmit(event) { 
+function onSubmit(event) {
   event.preventDefault();
-  queryWord = refs.inputEl.value;
-  console.log(refs.inputEl.value);
-  getImage(queryWord, 1).then(linksArray => {renderHTML(linksArray)});
-  
-  };
+  getWord();
+  getImage(queryWord, 1).then(linksArray => {
+    renderHTML(linksArray);
+  });
+}
 
 function getWord(evt) {
-  let normalizedWord = evt.target.value.trim();
-  console.log(normalizedWord);
+  queryWord = refs.inputEl.value.trim();
+  console.log(refs.inputEl.value);
   // return normalizedWord;
 }
 
+function callLightbox() {
+  let lightbox = new SimpleLightbox('.gallery a', {
+    showCounter: true,
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
 
-// const galleryTabsContainer = galleryItems.map(image => {
-//   return ` <li><a class = "gallery__item" href = "${image.original}">
-// <img class = "gallery__image" alt = "${image.description}" src = "${image.preview}"></a></li>`;
-// });
-
-// galleryEl.insertAdjacentHTML('afterbegin', galleryTabsContainer.join(''));
-
-// // console.log(galleryEl);
+refs.formEl.addEventListener('submit', onSubmit);
 
 
-// let lightbox = new SimpleLightbox('.gallery a', {
-//   showCounter: true,
-//   captions: true,
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
-
-// key 22104578-b37830bb47769ec8fcc7503cc
