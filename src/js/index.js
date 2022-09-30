10d630e4f1cb6a167c3aa58b14fb715f479fc3cd
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-30 22:30:01 +0300

    add notifications fix loadmore btn

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,10 @@ const refs = {
 let queryWord = '';
 let pageNumber = 1;
 
+refs.inputEl.addEventListener('input', clearHTML);
+refs.formEl.addEventListener('submit', onSubmit);
+refs.loadMoreBtnEl.addEventListener('click', onLoadMore);
+
 async function getImageData(word, pageNumber) {
   try {
     const response = await axios.get(
@@ -26,23 +30,55 @@ async function getImageData(word, pageNumber) {
           q: word,
           orientation: 'horizontal',
           safesearch: true,
-
-        }
+        },
       }
-  
-      );
+    );
     // console.log(response.data.hits);
     const linksArray = response.data.hits;
+    if (linksArray.length > 0) {
+      Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+    }
+    if (linksArray.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    console.log('getImage', linksArray);
     return linksArray;
   } catch (error) {
-    console.error(error);
+    console.error('ERROR');
   }
 }
 
-function showButton () {
+async function onLoadMore() {
+  pageNumber += 1;
+  const linksArray = await getImageData(queryWord, pageNumber);
+  if (linksArray < 1) {
+    hideButton();
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+  renderHTML(linksArray);
+  callLightbox();
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+  getWord();
+  const linksArray = await getImageData(queryWord, pageNumber);
+  await renderHTML(linksArray);
+  callLightbox();
+  if (linksArray.length > 0) {
+    showButton();
+  }
+}
+
+function showButton() {
   refs.loadMoreBtnEl.classList.remove('is-hidden');
 }
 
+function hideButton() {
+  refs.loadMoreBtnEl.classList.add('is-hidden');
+}
 
 function renderHTML(array) {
   const imagesMarkup = array.map(image => {
@@ -69,18 +105,7 @@ function renderHTML(array) {
   </div>`;
   });
   refs.galleryEl.insertAdjacentHTML('beforeend', imagesMarkup.join(''));
-  callLightbox();
-  //  console.log(imagesMarkup);
 }
-
-async function onSubmit(event) {
-  event.preventDefault();
-  getWord();
-  const linksArray = await getImageData(queryWord, pageNumber);
-  renderHTML(linksArray);
-  showButton();
-  }
-
 
 function getWord(evt) {
   queryWord = refs.inputEl.value.trim();
@@ -94,21 +119,11 @@ function callLightbox() {
     captions: true,
     captionsData: 'alt',
     captionDelay: 250,
-  }); 
+  });
 }
 
-function clearHTML () {
-  refs.galleryEl.innerHTML = ' '
+function clearHTML() {
+  refs.galleryEl.innerHTML = ' ';
+  pageNumber = 1;
+  hideButton();
 }
-
-
-async function onLoadMore() {
-  pageNumber +=1;
-  const linksArray = await getImageData(queryWord, pageNumber);
-  renderHTML(linksArray);};
-
-refs.inputEl.addEventListener('input', clearHTML);
-refs.formEl.addEventListener('submit', onSubmit);
-refs.loadMoreBtnEl.addEventListener('click', onLoadMore);
-
-
